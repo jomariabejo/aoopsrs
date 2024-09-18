2fc8e405db9d52f6adaaba418511e1a0c90bf669
Jomari S. Abejo <70135786+jomariabejo@users.noreply.github.com>
2024-09-18 13:13:42 +0800

    update home, added purpose, scope, exclusions

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,9 @@
 
 <template>
   <main>
-    <div class="flex flex-col lg:relative text-center lg:flex lg:items-center lg:justify-center">
+    <div
+      class="flex flex-col lg:relative text-center lg:flex lg:items-center lg:justify-center shadow-black"
+    >
       <img
         class="w-full max-h-screen object-cover opacity-70"
         src="https://images.pexels.com/photos/1413412/pexels-photo-1413412.jpeg?cs=srgb&dl=pexels-giorgio-de-angelis-482403-1413412.jpg&fm=jpg"
@@ -16,6 +18,48 @@
         >
           Expanded MotorPH Payroll System Software Requirements Specification
         </h1>
+      </div>
+    </div>
+    <div class="flex flex-col items-center lg:flex-row lg:items-start lg:px-16">
+      <div
+        class="hover:bg-gray-800 duration-300 rounded-md hover:text-white container text-center my-16 p-4"
+      >
+        <h2 class="text-4xl font-bold my-4">Purpose</h2>
+        <p class="text-justify text-2xl tracking-wide">
+          The purpose of creating the Software Requirements Specification (SRS) is to provide a
+          comprehensive and detailed description of the payroll system. This document aims to ensure
+          that the system meets the intended requirements, and serves as a reference for the
+          development, maintenance, and enhancement of the system. The SRS covers both functional
+          and non-functional requirements and includes sections on project scope, product
+          perspective, user needs, assumptions, dependencies, system features, and non-functional
+          requirements. Key stakeholders, such as project managers, developers, testers, and
+          end-users, will use this document to understand and implement the system accurately.
+        </p>
+      </div>
+      <div
+        class="hover:bg-gray-800 duration-300 rounded-md hover:text-white container text-center my-16 p-4"
+      >
+        <h2 class="text-4xl font-bold my-4">Scope</h2>
+        <p class="text-justify text-2xl tracking-wide">
+          The updated payroll system aims to enhance employee management and payroll processing
+          capabilities. The system will provide a user-friendly interface and includes features such
+          as employee management, payroll calculation, leave management, reporting, and a
+          self-service portal for employees. The primary objectives are to improve efficiency,
+          accuracy, and compliance with regulatory requirements. This system supports the corporate
+          goal of enhancing operational efficiency and employee satisfaction.
+        </p>
+      </div>
+
+      <div
+        class="hover:bg-gray-800 duration-300 rounded-md hover:text-white container text-center my-16 p-4"
+      >
+        <h2 class="text-4xl font-bold my-4">Exclusion</h2>
+        <p class="text-justify text-2xl tracking-wide">
+          The payroll system does not extend to employee performance evaluations, goal setting, or
+          other aspects of performance management. Additionally, integration with external
+          third-party software, non-payroll HR functions such as recruitment, and multi-currency
+          payroll processing are excluded from this system.
+        </p>
       </div>
     </div>
   </main>
--- src/views/NavigationBarView.vue
+++ src/views/NavigationBarView.vue
@@ -4,14 +4,15 @@
   <nav class="bg-white border-gray-200 dark:bg-gray-900">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <RouterLink to="/" class="flex items-center space-x-3 rtl:space-x-reverse">
-        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
+        <span
+          class="font-sans font-extrabold text-slate-900 hover:text-slate-700 self-center text-2xl whitespace-nowrap dark:text-white"
           >MotorPH</span
         >
       </RouterLink>
       <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
         <button
           type="button"
-          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          class="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Download
         </button>
@@ -51,7 +52,7 @@
           <li>
             <RouterLink
               to="/overall-description"
-              class="block py-2 px-3 md:p-0 text-gray-900 text-gray-900 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500"
+              class="block py-2 px-3 md:p-0 text-gray-900 rounded md:bg-transparent md:text-gray-700 hover:underline md:dark:text-blue-500"
               aria-current="page"
               >Overall Description</RouterLink
             >
@@ -59,28 +60,28 @@
           <li>
             <RouterLink
               to="/specific-requirements"
-              class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-gray-700 hover:underline md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >Specific Requirements</RouterLink
             >
           </li>
           <li>
             <RouterLink
               to="/system-architecture"
-              class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-gray-700 hover:underline md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >System Architecture</RouterLink
             >
           </li>
           <li>
             <RouterLink
               to="/appendices"
-              class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-gray-700 hover:underline md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >Appendices</RouterLink
             >
           </li>
           <li>
             <RouterLink
               to="/contributor"
-              class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-gray-700 hover:underline md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >Contributor</RouterLink
             >
           </li>
