7df356779ee8b8f5db131b3981d245105979f644
Jomari S. Abejo <70135786+jomariabejo@users.noreply.github.com>
2024-09-18 09:03:09 +0800

    implement home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,8 +2,21 @@
 
 <template>
   <main>
-    <div class="font-bold">
-      Software Requirements Specification for MotorPH Payroll System Expansion
+    <div class="relative text-center flex items-center justify-center">
+      <img
+        class="w-full max-h-screen object-cover opacity-70"
+        src="https://images.pexels.com/photos/1413412/pexels-photo-1413412.jpeg?cs=srgb&dl=pexels-giorgio-de-angelis-482403-1413412.jpg&fm=jpg"
+        alt="motorcycle photo"
+      />
+      <div
+        class="absolute left-1/2 transform -translate-x-1/2 bg-gray-50 hover:bg-gray-100 duration-100 p-4 rounded-lg"
+      >
+        <h1
+          class="animate-pulse text-5xl p-4 text-center font-bold from-gray-800 via-gray-500 to-gray-300 bg-gradient-to-r bg-gradient-to-r bg-clip-text text-transparent max-w-screen-lg"
+        >
+          Expanded MotorPH Payroll System Software Requirements Specification
+        </h1>
+      </div>
     </div>
   </main>
 </template>
--- src/views/NavigationBarView.vue
+++ src/views/NavigationBarView.vue
@@ -1,28 +1,42 @@
 <script lang="ts"></script>
 
 <template>
-  <nav class="flex justify-between lg:p-4 bg-gray-100">
+  <nav class="flex justify-between items-center lg:p-4 bg-gray-100 shadow-slate-700">
     <div>
-      <RouterLink class="text-3xl font-extrabold text-gray-600" to="/">MotorPH</RouterLink>
+      <RouterLink
+        class="text-3xl font-extrabold text-gray-600 hover:text-gray-900 duration-150 hover:tracking-wide"
+        to="/"
+        >MotorPH</RouterLink
+      >
     </div>
     <div class="flex items-center justify-center gap-4">
       <!-- Introduction including: {Purpuse,Scope,Defitions,Acronyms,Abbreviations, References, Overview} -->
       <!-- <RouterLink to="/introduction">Introduction</RouterLink> -->
       <!-- Overall Description: Product perspective, Product function, user classes and characteristics, operating environment, Design and Implementation Constrains, Assumptions and Dependencies -->
-      <RouterLink class="hover:underline duration-150" to="/overall-description"
+      <RouterLink
+        class="hover:underline duration-150 hover:tracking-wider"
+        to="/overall-description"
         >Overall Description</RouterLink
       >
       <!-- Specific Requirements: Functional Requirements,Non-Functional Requirements ,External Interface Requirements,System Features,Use Cases,Data Requirements-->
-      <RouterLink class="hover:underline duration-150" to="/specific-requirements"
+      <RouterLink
+        class="hover:underline duration-150 hover:tracking-wider"
+        to="/specific-requirements"
         >Specific Requirements</RouterLink
       >
       <!-- System Architecture: Overview, Component Diagrams, Data Flow Diagrams, Design Considerations -->
-      <RouterLink class="hover:underline duration-150" to="/system-architecture"
+      <RouterLink
+        class="hover:underline duration-150 hover:tracking-wider"
+        to="/system-architecture"
         >System Architecture</RouterLink
       >
       <!-- Appendices: Glossary, List of figures and tables, change history -->
-      <RouterLink class="hover:underline duration-150" to="/appendices">Appendices</RouterLink>
-      <RouterLink class="hover:underline duration-150" to="/contributor">Contributor</RouterLink>
+      <RouterLink class="hover:underline duration-150 hover:tracking-wider" to="/appendices"
+        >Appendices</RouterLink
+      >
+      <RouterLink class="hover:underline duration-150 hover:tracking-wider" to="/contributor"
+        >Contributor</RouterLink
+      >
     </div>
   </nav>
 </template>
